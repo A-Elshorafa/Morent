1c7de031fcfc1d53b899d7008fb7757038bbdb87
Abdelrahman Elshorafa <abdelrahman_elshorafa@oulook.com>
2026-01-18 12:25:34 +0200

    feat(CarTransactions): add DTO

---
+++ src/Morent.UseCases/DTOs/CarReviewDto.cs
@@ -0,0 +1,43 @@
+namespace Morent.UseCases.DTOs;
+
+public class CreateCarReviewDto
+{
+  public int? CarId { get; set; }
+  public int? UserId { get; set; }
+  public int? Rating { get; set; }
+  public string? ReviewText { get; set; } = null!;
+  
+  public CreateCarReviewDto(int? carId = null, int? userId= null, int? rating= null, string? reviewText= null)
+  {
+    CarId = carId;
+    UserId = userId;
+    Rating = rating;
+    ReviewText = reviewText;
+  }
+}
+
+public class UpdateCarReviewDto : CreateCarReviewDto
+{
+  public int? CarReviewId { get; set; }
+
+  public UpdateCarReviewDto(int? carId = null, int? userId= null, int? rating= null, string? reviewText= null, int? carReviewId= null) 
+  {
+    CarId = carId;
+    UserId = userId;
+    Rating = rating;
+    ReviewText = reviewText;
+    CarReviewId = carReviewId;
+  }
+}
+
+public class GetCarReviewDto : UpdateCarReviewDto
+{
+  public GetCarReviewDto(int carId, int userId, int rating, string reviewText, int carReviewId) : base(carId, userId, rating, reviewText, carReviewId)
+  {
+    CarId = carId;
+    UserId = userId;
+    Rating = rating;
+    ReviewText = reviewText;
+    CarReviewId = carReviewId;
+  }
+}
---
+++ src/Morent.UseCases/DTOs/CarTransactionDto.cs
@@ -0,0 +1,161 @@
+namespace Morent.UseCases.DTOs;
+
+public abstract class CarTransactionBaseDto
+{
+  public DateTime FromDate { get; set; }
+  public DateTime ToDate { get; set; }
+  public decimal RentalPrice { get; set; }
+
+  public string? CardNumber { get; set; }
+  public string? CardHolderName { get; set; }
+  public DateTime CardExpiryDate { get; set; }
+
+  public string? PromoCode { get; set; }
+  public string? SubTotal { get; set; }
+
+  public int CarId { get; set; }
+  public int RenterId { get; set; }
+  public int PickupLocationId { get; set; }
+  public int DropOfLocationId { get; set; }
+  
+  public CarTransactionBaseDto(
+    DateTime? fromDate = null,
+    DateTime? toDate = null,
+    decimal? rentalPrice = null,
+    string? cardNumber = null,
+    string? cardHolderName = null,
+    DateTime? cardExpiryDate = null,
+    string? promoCode = null,
+    string? subTotal = null,
+    int? carId = null,
+    int? renterId = null,
+    int? pickupLocationId = null,
+    int? dropOfLocationId = null)
+  {
+    if (fromDate.HasValue) FromDate = fromDate.Value;
+    if (toDate.HasValue) ToDate = toDate.Value;
+    if (rentalPrice.HasValue) RentalPrice = rentalPrice.Value;
+
+    CardNumber = cardNumber;
+    CardHolderName = cardHolderName;
+    if (cardExpiryDate.HasValue) CardExpiryDate = cardExpiryDate.Value;
+
+    PromoCode = promoCode;
+    SubTotal = subTotal;
+
+    if (carId.HasValue) CarId = carId.Value;
+    if (renterId.HasValue) RenterId = renterId.Value;
+    if (pickupLocationId.HasValue) PickupLocationId = pickupLocationId.Value;
+    if (dropOfLocationId.HasValue) DropOfLocationId = dropOfLocationId.Value;
+  }
+}
+
+public class CreateCarTransactionDto : CarTransactionBaseDto
+{
+  public CreateCarTransactionDto(
+    DateTime? fromDate = null,
+    DateTime? toDate = null,
+    decimal? rentalPrice = null,
+    string? cardNumber = null,
+    string? cardHolderName = null,
+    DateTime? cardExpiryDate = null,
+    string? promoCode = null,
+    string? subTotal = null,
+    int? carId = null,
+    int? renterId = null,
+    int? pickupLocationId = null,
+    int? dropOfLocationId = null)
+  {
+    if (fromDate.HasValue) FromDate = fromDate.Value;
+    if (toDate.HasValue) ToDate = toDate.Value;
+    if (rentalPrice.HasValue) RentalPrice = rentalPrice.Value;
+
+    CardNumber = cardNumber;
+    CardHolderName = cardHolderName;
+    if (cardExpiryDate.HasValue) CardExpiryDate = cardExpiryDate.Value;
+
+    PromoCode = promoCode;
+    SubTotal = subTotal;
+
+    if (carId.HasValue) CarId = carId.Value;
+    if (renterId.HasValue) RenterId = renterId.Value;
+    if (pickupLocationId.HasValue) PickupLocationId = pickupLocationId.Value;
+    if (dropOfLocationId.HasValue) DropOfLocationId = dropOfLocationId.Value;
+  }
+}
+
+public class UpdateCarTransactionDto
+{
+  public int? CarTransactionId { get; set; }
+  public DateTime FromDate { get; set; }
+  public DateTime ToDate { get; set; }
+  public int PickupLocationId { get; set; }
+  public int DropOfLocationId { get; set; }
+  
+  public UpdateCarTransactionDto(
+    int? carTransactionId = null,
+    DateTime? fromDate = null,
+    DateTime? toDate = null,
+    int? pickupLocationId = null,
+    int? dropOfLocationId = null)
+  {
+    CarTransactionId = carTransactionId;
+    if (fromDate.HasValue) FromDate = fromDate.Value;
+    if (toDate.HasValue) ToDate = toDate.Value;
+
+    if (pickupLocationId.HasValue) PickupLocationId = pickupLocationId.Value;
+    if (dropOfLocationId.HasValue) DropOfLocationId = dropOfLocationId.Value;
+  }
+}
+
+
+public class CarTransactionDetailsDto : CarTransactionBaseDto
+{
+  public int? CarTransactionId { get; set; }
+
+  public DateTime CreatedAt { get; set; }
+  public DateTime? LastModifiedAt { get; set; }
+  
+  public CarTransactionDetailsDto(
+    int? carTransactionId = null,
+    DateTime? fromDate = null,
+    DateTime? toDate = null,
+    decimal? rentalPrice = null,
+    string? cardNumber = null,
+    string? cardHolderName = null,
+    DateTime? cardExpiryDate = null,
+    string? promoCode = null,
+    string? subTotal = null,
+    int? carId = null,
+    int? renterId = null,
+    int? pickupLocationId = null,
+    int? dropOfLocationId = null,
+    DateTime? createdAt = null,
+    DateTime? lastModifiedAt = null
+  )
+  {
+    CarTransactionId = carTransactionId;
+    if (fromDate.HasValue) FromDate = fromDate.Value;
+    if (toDate.HasValue) ToDate = toDate.Value;
+    if (rentalPrice.HasValue) RentalPrice = rentalPrice.Value;
+
+    CardNumber = cardNumber;
+    CardHolderName = cardHolderName;
+    if (cardExpiryDate.HasValue) CardExpiryDate = cardExpiryDate.Value;
+
+    PromoCode = promoCode;
+    SubTotal = subTotal;
+
+    if (carId.HasValue) CarId = carId.Value;
+    if (renterId.HasValue) RenterId = renterId.Value;
+    if (pickupLocationId.HasValue) PickupLocationId = pickupLocationId.Value;
+    if (dropOfLocationId.HasValue) DropOfLocationId = dropOfLocationId.Value;
+    if (createdAt.HasValue) CreatedAt = createdAt.Value;
+    if (lastModifiedAt.HasValue) LastModifiedAt = lastModifiedAt.Value;
+  }
+}
+
+public class DeleteCarTransactionDto
+{
+  public int CarTransactionId { get; set; }
+}
